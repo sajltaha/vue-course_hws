c9c0d5ad3be1de1107551ae37ab46c96061f1a5c
sajltaha <s.t.a.r.n.k06@gmail.com>
2023-10-23 02:52:32 +0600

    hw

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<script>
+import Slide from "./components/Slide.vue";
+export default {
+  components: {
+    Slide,
+  },
+  data: () => {
+    return {
+      slides: [
+        {
+          id: 1,
+          title: "Slide 1",
+          description: "This is slide 1",
+        },
+        {
+          id: 2,
+          title: "Slide 2",
+          description: "This is slide 2",
+        },
+        {
+          id: 3,
+          title: "Slide 3",
+          description: "This is slide 3",
+        },
+        {
+          id: 4,
+          title: "Slide 4",
+          description: "This is slide 4",
+        },
+        {
+          id: 5,
+          title: "Slide 5",
+          description: "This is slide 5",
+        },
+        {
+          id: 6,
+          title: "Slide 6",
+          description: "This is slide 6",
+        },
+      ],
+      index: 0,
+    };
+  },
+  methods: {
+    onForward() {
+      if (this.index == 5) {
+        this.index = this.index * 0;
+      } else {
+        this.index = this.index + 1;
+      }
+    },
+    onBack() {
+      if (this.index == 0) {
+        this.index = this.index + 5;
+      } else {
+        this.index = this.index - 1;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <Slide
+      :key="slides[index].id"
+      :title="slides[index].title"
+      :description="slides[index].description"
+    />
+    <div class="buttons">
+      <button @click="onBack(index)">Назад</button>
+      <button @click="onForward(index)">Вперед</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 900px;
+  height: 500px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  justify-content: center;
+}
+
+.buttons {
+  display: flex;
+  gap: 10px;
+}
+</style>
---
+++ src/components/Slide.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: ["title", "description"],
+  data: () => {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="slide">
+    <h1>{{ title }}</h1>
+    <h3>{{ description }}</h3>
+  </div>
+</template>
+
+<style scoped>
+.slide {
+  width: 500px;
+  height: 300px;
+  border: 1px solid black;
+  color: black;
+  gap: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
